64f82c9fe248c538fa85fe6b943713027cb06f71
Matteo Galacci <m.galacci@gmail.com>
2023-02-24 11:19:24 +0100

    improves docker setup

--- docker/data/db/sql-schema.sql
+++ docker/data/db/sql-schema.sql
@@ -1,4 +1,8 @@
-create table events
+CREATE DATABASE IF NOT EXISTS memo_db;
+
+USE memo_db;
+
+create table if not exists memo_db.events
 (
     id int auto_increment primary key,
     uuid        char(36)     not null,
@@ -10,8 +14,11 @@ create table events
     constraint UNIQ_5387574AD17F50A634B91FA9 unique (uuid, playhead)
 );
 
-create table memo(
+create table if not exists memo_db.memo(
     id char(36) primary key,
     body longtext     not null,
     created_at varchar(255)  not null
 );
+
+TRUNCATE TABLE memo_db.events;
+TRUNCATE TABLE memo_db.memo;
--- readme.md
+++ readme.md
@@ -9,6 +9,13 @@ go mod tidy
 ```
 
 ### Test
-```bash
+
+`cp .env .env.local` and set your DSN
+
+`cp docker/docker-compose.override.dist.yml docker/docker-compose.override.yml` and set your DB port
+
+``` bash
+make upd
+make prepare-db
 go test -v ./...
 ```
